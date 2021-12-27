4e08bba35d755a514b1b8b11af55968fb89508b1
Josh Leveck <joshleveck@gmail.com>
2021-12-27 14:39:30 -0500

    finished the blockchain

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "Blockchain.h"
+#include <iostream>
+#include "sha256.h"
+#include <sstream>
+
+int main()
+{
+    Blockchain block_chain = Blockchain();
+    std::cout << "Mining block 1..." << std::endl;
+    block_chain.add_block(Block(1, "Block 1 Data"));
+
+    std::cout << "Mining block 2..." << std::endl;
+    block_chain.add_block(Block(2, "Block 2 Data"));
+
+    std::cout << "Mining block 3..." << std::endl;
+    block_chain.add_block(Block(3, "Block 3 Data"));
+
+    std::vector<Block> chain = block_chain.get_chain();
+
+    for (int i = 0; i < chain.size(); i++)
+    {
+        std::cout << i << ". " << chain[i].get_data() << std::endl;
+        std::cout << chain[i].get_hash() << std::endl;
+    }
+
+    return 0;
+}
